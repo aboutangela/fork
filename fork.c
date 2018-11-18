de3c4fc9632198268b2bac4b3621ab4105c98673
angela <ali18@stuy.edu>
2018-11-18 11:40:13 -0500

    edited files

--- fork.c
+++ fork.c
@@ -2,26 +2,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 int main(){
+  srand(time(NULL));
   printf("parent process \n");
   int one = fork();
-  int two = fork();
-  if(getpid() == one){
+  if(! one){
+    int t1 = rand() % 15 + 5;
     printf("one pid: %d \n", getpid());
-    sleep(1);
+    sleep(t1);
     printf("one finished \n");
-    return 1;
+    return t1;
   }
-  if(getpid() == two){
-    printf("two pid: %d \n", getpid());
-    sleep(2);
-    printf("one finished \n");
-    return 2;
+  else{
+    int two = fork();
+    if(! two){
+      int t2 = rand() % 3 + 5;
+      printf("two pid: %d \n", getpid());
+      sleep(t2);
+      printf("two finished \n");
+      return t2;
+    }
+    else{
+      int s;
+      int p = wait(&s);
+      printf("completed child pid: %d, seconds asleep: %d \n", p, WEXITSTATUS(s));
+      return 0;
+    }
   }
-  int s;
-  wait(s);
-  printf("completed child pid: %d, seconds asleep: %d \n")
-  return 0;
 }
-
